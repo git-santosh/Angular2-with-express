6645af3820dad53e306f7b5a8ef75590598b1002
santosh suryawanshi <santosh.suryawanshi28@gmail.com>
2017-05-21 03:33:20 +0530

    Created another component called courses

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { CoursesComponent } from "./courses.component";
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+    <courses></courses>`
 })
 export class AppComponent  { name = 'Angular'; }
