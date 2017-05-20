86975b18863803ea45139d2deea43edc2f218d99
santosh suryawanshi <santosh.suryawanshi28@gmail.com>
2017-05-21 01:18:43 +0530

    added src folder(altered) for running live-server

--- client/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app',
-  template: `<h1>Hello from Angular</h1>`
-})
-export class AppComponent {  }
--- client/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-var platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
