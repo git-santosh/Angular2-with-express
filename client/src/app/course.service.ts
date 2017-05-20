bc06ff79d27cf38ad083b9c7342704eec3110538
santosh suryawanshi <santosh.suryawanshi28@gmail.com>
2017-05-21 03:50:46 +0530

    Created service for redering course list

---
+++ client/src/app/course.service.ts
@@ -0,0 +1,6 @@
+
+export class CourseService {
+    getCourses() : string []{ //explicitly write return type like getCourses() : string [] {}
+        return ["course1","course2","course3"];
+    }
+}
